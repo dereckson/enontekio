6aa98e99b74b99163eae24bdf81db8137039d7e4
Sébastien Santoro <dereckson@espace-win.org>
2022-12-08 13:32:36 +0100

    Parse a file line by line into a collection

---
+++ README.md
@@ -0,0 +1,17 @@
+# Enontekio
+
+Enontekio is a Rust library containing utilities frequently used
+to solve problems with data extraction and manipulation, like
+Advent of Code puzzles.
+
+Enontekïo is a city in Lapland, and as Advent of Code uses
+the elves' myth, that was a suitable name for this library.
+
+## Parser
+
+Those functions allow to parse a file line by line,
+and map lines to specific types.
+
+## License
+
+Licensed under BSD-2-Clause.
---
+++ src/parser/mod.rs
@@ -0,0 +1,93 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::io::Error as IOError;
+use std::path::Path;
+
+/// Attempts to open and parse a file line by line into a collection through a callback function.
+///
+/// The callback function will be used as a map and receive Result<string> as parameter.
+/// The type should be compatible with collect to build a collection.
+pub fn parse_file_by_line<P, T, C>(filename: P, callback: fn(Result<String, IOError>) -> T) -> Result<C, IOError>
+    where P: AsRef<Path>, C: FromIterator<T> {
+    let fd = File::open(filename)?;
+    let lines = BufReader::new(fd).lines();
+
+    Ok(lines
+        .map(callback)
+        .collect())
+}
+
+/// Attempts to open and parse a file containing digits into a vector of u32 vectors
+///
+/// For example, a file `digits.dat` with:
+///     123455
+///     891245
+///
+/// ```
+/// use enontekio::parser;
+///
+/// let expected_digits = vec![vec![1 as u32, 2, 3, 4, 5, 5], vec![8, 9, 1, 2, 4, 5]];
+/// let actual_digits = parser::parse_digits_grid_file("tests/parser/digits.dat").unwrap();
+/// assert_eq!(actual_digits, expected_digits);
+/// ```
+pub fn parse_digits_grid_file<P>(filename: P) -> Result<Vec<Vec<u32>>, IOError> where P: AsRef<Path> {
+    parse_file_by_line(filename, |line| parse_digits_grid_line(&line.unwrap()).unwrap())
+}
+
+/// Parses a string into a vector of digits
+///
+/// ```
+/// use enontekio::parser;
+///
+/// let expected_digits = vec![1 as u32, 2, 3, 4, 5];
+/// let actual_digits = parser::parse_digits_grid_line("12345").unwrap();
+/// assert_eq!(actual_digits, expected_digits);
+/// ```
+pub fn parse_digits_grid_line(line: &str) -> Option<Vec<u32>> {
+    line
+        .chars()
+        .map(|c| c.to_digit(10))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_parse_file_by_line() {
+        let expected = vec!['@', 'A', 'B', 'C'];
+        let actual: Vec<_> = parse_file_by_line(
+            "tests/parser/ascii.txt",
+            |line| line.unwrap().parse::<u8>().unwrap() as char
+        ).unwrap();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_parse_file_by_line_with_non_existing_file() {
+        let result: Result<Vec<_>, _> = parse_file_by_line("/nonexisting", |_| ());
+
+        assert_eq!(true, result.is_err());
+    }
+
+    #[test]
+    fn test_parse_digits_grid_file() {
+        let expected_digits = vec![vec![1 as u32, 2, 3, 4, 5, 5], vec![8, 9, 1, 2, 4, 5]];
+        let actual_digits = parse_digits_grid_file("tests/parser/digits.dat").unwrap();
+
+        assert_eq!(actual_digits, expected_digits);
+    }
+
+    #[test]
+    fn test_parse_digits_grid_line() {
+        assert_eq!(vec![1 as u32, 2, 3, 4, 5], parse_digits_grid_line("12345").unwrap());
+    }
+
+    #[test]
+    fn parse_digits_grid_line_when_it_is_not() {
+        assert_eq!(None, parse_digits_grid_line("This is not a digits line."));
+    }
+}
